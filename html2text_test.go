66c9ccdce175bc388a249ca3149edd94aff4d156
Kaur Kuut <strom@nevermore.ee>
2023-05-05 00:08:10 +0300

    Add more robust support for HTML5 anchor tags.

--- html2text_test.go
+++ html2text_test.go
@@ -15,6 +15,13 @@ func TestHTML2Text(t *testing.T) {
 
 			// the original behavior
 			So(HTML2Text(`click <a href="test">here</a>`), ShouldEqual, "click test")
+			So(HTML2Text(`click <A hRef="test">here</A>`), ShouldEqual, "click test")
+			So(HTML2Text(`click <a href='test'>here</a>`), ShouldEqual, "click test")
+			So(HTML2Text(`click <a href=test>here</a>`), ShouldEqual, "click test")
+			So(HTML2Text(`click <a href =test>here</a>`), ShouldEqual, "click test")
+			So(HTML2Text(`click <a href = test>here</a>`), ShouldEqual, "click test")
+			So(HTML2Text(`click <a href      =     test>here</a>`), ShouldEqual, "click test")
+			So(HTML2Text(`click <a href      =     test target="_blank">here</a>`), ShouldEqual, "click test")
 			So(HTML2Text(`click <a class="x" href="test">here</a>`), ShouldEqual, "click test")
 			So(HTML2Text(`click <a href="ents/&apos;x&apos;">here</a>`), ShouldEqual, "click ents/'x'")
 			So(HTML2Text(`click <a href="javascript:void(0)">here</a>`), ShouldEqual, "click ")
@@ -24,6 +31,13 @@ func TestHTML2Text(t *testing.T) {
 
 			// with inner text
 			So(HTML2TextWithOptions(`click <a href="test">here</a>`, WithLinksInnerText()), ShouldEqual, "click here <test>")
+			So(HTML2TextWithOptions(`click <A hRef="test">here</A>`, WithLinksInnerText()), ShouldEqual, "click here <test>")
+			So(HTML2TextWithOptions(`click <a href='test'>here</a>`, WithLinksInnerText()), ShouldEqual, "click here <test>")
+			So(HTML2TextWithOptions(`click <a href=test>here</a>`, WithLinksInnerText()), ShouldEqual, "click here <test>")
+			So(HTML2TextWithOptions(`click <a href =test>here</a>`, WithLinksInnerText()), ShouldEqual, "click here <test>")
+			So(HTML2TextWithOptions(`click <a href = test>here</a>`, WithLinksInnerText()), ShouldEqual, "click here <test>")
+			So(HTML2TextWithOptions(`click <a href      =     test>here</a>`, WithLinksInnerText()), ShouldEqual, "click here <test>")
+			So(HTML2TextWithOptions(`click <a href      =     test target="_blank">here</a>`, WithLinksInnerText()), ShouldEqual, "click here <test>")
 			So(HTML2TextWithOptions(`click <a class="x" href="test">here</a>`, WithLinksInnerText()), ShouldEqual, "click here <test>")
 			So(HTML2TextWithOptions(`click <a href="ents/&apos;x&apos;">here</a>`, WithLinksInnerText()), ShouldEqual, "click here <ents/'x'>")
 			So(HTML2TextWithOptions(`click <a href="javascript:void(0)">here</a>`, WithLinksInnerText()), ShouldEqual, "click here")
@@ -87,6 +101,7 @@ func TestHTML2Text(t *testing.T) {
 		Convey("Full HTML structure", func() {
 			So(HTML2Text(``), ShouldEqual, "")
 			So(HTML2Text(`<html><head><title>Good</title></head><body>x</body>`), ShouldEqual, "x")
+			So(HTML2Text(`<html><head href="foo"><title>Good</title></head><body>x</body>`), ShouldEqual, "x")
 			So(HTML2Text(`we are not <script type="javascript"></script>interested in scripts`),
 				ShouldEqual, "we are not interested in scripts")
 		})
